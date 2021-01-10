27b939e68d9cabdd6dd31f03d136c972175bd19c
Tim Cheung <timshingyu@gmail.com>
2021-01-10 16:46:13 +0800

    Added sales report endpoint

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express"
+import { endOfDay, startOfDay } from 'date-fns'
 
 import formidable from 'formidable'
 import fs from 'fs'
+import isBefore from 'validator/lib/isBefore'
+import isDate from 'validator/lib/isDate'
 import parse from 'csv-parse'
 import prisma from '../src/lib/prisma'
 
@@ -85,4 +88,79 @@ export async function uploadSalesRecord (req: Request, res: Response, next: Next
   processFile(csvTempFile.path)
     
   res.json({ message: 'ok' })
+}
+
+type TValidate = {
+  date?: string
+  startDate?: string
+  endDate?: string
+}
+
+function validate({ date, startDate, endDate }: TValidate) {
+  if (!date && !(startDate && endDate)) {
+    throw new Error('Must provide either singe date or date range')
+  }
+
+  if (date && !isDate(date)) {
+    throw new Error('Must provide valid date e.g 2021/01/17')
+  }
+
+  if (startDate && !isDate(startDate)) {
+    throw new Error('Must provide valid date e.g 2021/01/17')
+  }
+
+  if (endDate && !isDate(endDate)) {
+    throw new Error('Must provide valid date e.g 2021/01/17')
+  }
+
+  if (startDate && endDate && !isBefore(startDate, endDate)) {
+    throw new Error('Start date must before end date')
+  }
+}
+
+function getFilter({ date, startDate, endDate }: TValidate) {
+  let filter
+  if (date) {
+    filter = {
+      AND: [
+        {
+          lastPurchaseDate: {
+            gte: startOfDay(new Date(date)),
+          },
+        },
+        {
+          lastPurchaseDate: {
+            lte: endOfDay(new Date(date)),
+          },
+        },
+      ],
+    }
+  } else if (startDate && endDate) {
+    filter = {
+      AND: [
+        {
+          lastPurchaseDate: {
+            gte: new Date(startDate),
+          },
+        },
+        {
+          lastPurchaseDate: {
+            lte: new Date(endDate),
+          },
+        },
+      ],
+    }
+  }
+  return filter
+}
+
+export async function viewSalesRecord(req: Request, res: Response, next: NextFunction) {
+  const formData = await formParse(req)
+  validate(formData.fields)
+
+  const sales = await prisma.sale.findMany({
+    where: getFilter(formData.fields),
+  })
+  
+  res.json(sales)
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 import express, { Application } from "express";
-
-import { uploadSalesRecord } from './api'
+import { uploadSalesRecord, viewSalesRecord } from './api'
 
 const mount = async (app: Application) => {
   app.post('/sales/record', uploadSalesRecord)
+  app.post('/sales/report', viewSalesRecord)
 
   app.listen(process.env.PORT);
   console.log(`[app] : http://localhost:${process.env.PORT}`);
